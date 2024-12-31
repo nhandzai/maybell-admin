f81213f4eaa5ff8075ef89784ec47ef0131d39d3
nhandzai <ducnhan1522004@gmail.com>
2025-01-01 01:00:43 +0700

    Sort account by fullName, email, creatAt, isAdmin, isBan

--- components/account/account-model.js
+++ components/account/account-model.js
@@ -1,22 +1,37 @@
 const fs = require('fs').promises;
 const path = require('path');
-const limit = 6
+const limit = 6;
 
 async function getAccounts(req) {
     const filePath = path.join(__dirname, '../../data/account-list.json');
     const jsonData = await fs.readFile(filePath, 'utf8');
     const accountsData = JSON.parse(jsonData);
 
-    
     const page = parseInt(req.query.page) || 1;
     const emailFilter = req.query.email || '';
     const fullNameFilter = req.query.fullName || '';
+    const sortField = req.query.sortField || ''; 
+    const sortOrder = req.query.sortOrder || '';
 
     const filteredAccounts = accountsData.filter(account => {
-        const matchesEmail = emailFilter ? account.email.includes(emailFilter) : true;
-        const matchesFullName = fullNameFilter ? account.fullName.includes(fullNameFilter) : true;
+        const matchesEmail = emailFilter ? account.email.toLowerCase().includes(emailFilter.toLowerCase()) : true;
+        const matchesFullName = fullNameFilter ? account.fullName.toLowerCase().includes(fullNameFilter.toLowerCase()) : true;
         return matchesEmail && matchesFullName;
     });
+
+    if (sortField) {
+        filteredAccounts.sort((a, b) => {
+            const fieldA = a[sortField];
+            const fieldB = b[sortField];
+
+            if (sortOrder === 'asc') {
+                return fieldA > fieldB ? 1 : fieldA < fieldB ? -1 : 0;
+            } else if (sortOrder === 'desc') {
+                return fieldA < fieldB ? 1 : fieldA > fieldB ? -1 : 0;
+            }
+            return 0;
+        });
+    }
 
     const offset = (page - 1) * limit;
     const accounts = filteredAccounts.slice(offset, offset + limit);
