2ca7922e192390c432dfd7b69ad20f78e6ae398b
nhandzai <ducnhan1522004@gmail.com>
2025-01-03 16:58:59 +0700

    add new router

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const accountController = require('../components/account/account-controller');
 const productController = require('../components/product/product-controller');
 const profileController = require('../components/profile/profile-controller');
 
+router.get('/', profileController.getProfilePage)
 router.get('/accounts', accountController.getAccountPage);
 router.get('/products', productController.getProductPage);
 router.get('/profile', profileController.getProfilePage);
